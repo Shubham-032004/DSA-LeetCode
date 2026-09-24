f4c8e6d982a9848604cc15baaae206d2442a8b9c
shubham pandey <shubhampandey2736641@gmail.com>
2026-09-25 00:06:40 +0530

    Time: 462 ms (14.78%), Space: 16.6 MB (56.57%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,21 +1,38 @@
+# class Solution(object):
+#     def lengthOfLongestSubstring(self, s):
+
+#         left = 0
+#         chars = set()
+#         max_len = 0
+
+#         for right in range(len(s)):
+
+#             while s[right] in chars:
+#                 chars.remove(s[left])
+#                 left += 1
+
+#             chars.add(s[right])
+
+#             max_len = max(max_len, right - left + 1)
+
+#         return max_len
+
+
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
 
+        freq = {}
         left = 0
-        chars = set()
         max_len = 0
 
         for right in range(len(s)):
 
-            while s[right] in chars:
-                chars.remove(s[left])
-                left += 1
+            freq[s[right]] = freq.get(s[right], 0) + 1
 
-            chars.add(s[right])
+            while freq[s[right]] > 1:
+                freq[s[left]] -= 1
+                left += 1
 
             max_len = max(max_len, right - left + 1)
 
         return max_len
-
-
-        
